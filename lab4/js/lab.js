f572db80fdeb90834efa914e71170c0d48c81d58
nqhoang54 <146907877+nqhoang54@users.noreply.github.com>
2024-10-17 22:09:09 -0700

    lab 4 stuff

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -19,3 +19,55 @@ function main() {
 
 // let's get this party started
 main();
+
+
+// Rock Paper Scissors
+// An Implementation of the Game
+// Nam Hoang <nqhoang@ucsc.edu> 
+// 17 October 2024
+
+// task for today
+//      high priority
+//          finish twine game
+//                  finish formating/stylizing text 
+//                  add images and sound to the background
+//                  proofread the game check spelling etc
+//                  have someone playtest game
+//                  revise game
+//          finish 600 word essay using red dead 2
+//                  use lectures and zubek's readings
+//                  cite sources if possible
+//      low priority
+//          finish lecture
+//                  find the question
+//      playtest assignment
+//                  get someone to playtest
+
+// Rock Paper Scissors
+//      reset-round
+//          somehow reset the board every round
+//      inputs
+//          press button 1='rock'
+//          press button 2='paper'
+//          press button 3='scissors'
+//      computer-moves
+//          randomize between rock, paper, scissors
+//      win-conditions
+//            rock > scissors
+//            scissors > paper
+//            paper > rock
+//                    if true tplayer wins
+//                     if false fplayer loses
+//                    if else players tie
+
+//        objects
+//            rock
+//            paper
+//            scissors
+
+//        timer
+//            4 seconds to choose which object
+//        animations
+//            insert photos
+//            winning, losing, and tying animations
+//            animations for the buttons
