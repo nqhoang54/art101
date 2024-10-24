f7e04d4b075ab09402e0d3bd83cda19f6b92ab51
nqhoang54 <nqhoang@ucsc.edu>
2024-10-24 16:55:26 -0700

    lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -19,3 +19,33 @@ function main() {
 
 // let's get this party started
 main();
+/**
+ * Author:    Nam Hoang
+ * Created:   10.24.2024
+ * 
+ * Public Domain 
+ **/
+
+//Define Variables 
+const myTransport =['bus','walking','ride with friends'];
+
+//Create an object for my main ride
+var myMainRide ={
+    make: "Honda",
+    model: "Civic",
+    color: "blue",
+    year: 2004,
+    ownIt: true,
+    age : function(){
+      return 2022-this.year;
+    }};
+
+//Output for mytransport
+document.writeln("Getting around: " + myTransport + "<br>");
+
+//Output for my main ride
+document.writeln("My Main Ride: <pre>", 
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
+
+
+    
