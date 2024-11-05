e1f4692ac294f89aef97fe353530c48050ad7f53
nqhoang54 <146907877+nqhoang54@users.noreply.github.com>
2024-11-04 22:19:33 -0800

    lab 9

--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -6,11 +6,21 @@
  **/
 // add button to challenge section
 $("#challenge").append("<button id='button-challenge'>Make Special</button>");
-$("#problems").append("<button id='button-challenge'>Make Special</button>");
-$("#reflections").append("<button id='button-challenge'>Make Special</button>");
+$("#problems").append("<button id='button-problems'>Make Special</button>");
+$("#reflections").append("<button id='button-reflections'>Make Special</button>");
 
 // add a click listener to the challenge button
-$("button-challenge").click(function(){
+$("#button-challenge").click(function(){
     // now add (or subtract) the "special" class to the section
-    $("#challenge").toggleClass(".special");
+    $("#challenge").toggleClass("special");
+});
+
+$("#button-problems").click(function(){
+    // now add (or subtract) the "special" class to the section
+   $("#problems").toggleClass("special");
+});
+
+$("#button-reflections").click(function(){
+    // now add (or subtract) the "special" class to the section
+      $("#reflections").toggleClass("special");
 });
