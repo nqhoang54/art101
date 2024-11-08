1888a16dc8e354f9b8eee6990614197a4f1b99b8
nqhoang54 <nqhoang@ucsc.edu>
2024-11-07 16:13:38 -0800

    lebron

--- section/js/section.js
+++ section/js/section.js
@@ -1,18 +1,8 @@
-function userInput(){
-    let number=
-    window.prompt ('add number 0-9');
-    console.log(number);
-    return number;
-}
+//add a button
+$('h2').click()
 
+//add a listener 
+  //  function (){
+    //    $('.minor-section').toggleclass('party');
 
-let numberFirst = userInput();
-let numberSecond = userInput();
-
-console.log('number first is '+numberFirst)
-console.log('number first is' +numberSecond)
-
-results= numberFirst + numberSecond;
-document.writeln('Your numbers were '+numberFirst+' and '+numberSecond+' = ');
-$('.simple').append(results);
-console.log(results);
+   // }
